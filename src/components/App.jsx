150063e791d0fb710f28f7ea7eed59f6e443070c
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-03 10:58:59 +0300

    AppContainer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import '../style/App.css';
 import Button from './Button';
+import { AppContainer } from './AppContainer.style';
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Button />
-    </div>
+    </AppContainer>
   );
 }
 
---
+++ src/components/AppContainer.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+  margin-top: 3rem;
+  background-color: lightcoral;
+  min-height: 100vh;
+`;
